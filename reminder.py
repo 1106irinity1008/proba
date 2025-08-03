0decf1936e7dba1a7e00e1b92532ae54fc3762fe
1106irinity1008 <1106.irinity.1008@gmail.com>
2025-08-03 16:26:31 +0700

    Увеличен шрифт и убраны секунды

--- reminder.py
+++ reminder.py
@@ -16,7 +16,7 @@ def set():
             minute = int(rem.split(':')[1])
             now = datetime.datetime.now()
             print(now)
-            dt = now.replace(hour=hour, minute=minute)
+            dt = now.replace(hour=hour, minute=minute, second=0)
             print(dt)
             t = dt.timestamp()
             print(t)
@@ -40,12 +40,13 @@ def play_snd():
     pygame.mixer.music.play()
 
 
-
 window = Tk()
 window.title('Напоминание')
-label = Label(text='Установите напоминание')
+label = Label(text='Установите напоминание', font='Arial 14')
 label.pack(pady=10)
 set_button = Button(text='Установить напоминание', command=set)
 set_button.pack()
+
+check()
 
 window.mainloop()
