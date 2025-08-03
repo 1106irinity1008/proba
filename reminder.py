c38784405f64bc714f17aefe6b4cfdc0e9ff3b6e
1106irinity1008 <1106.irinity.1008@gmail.com>
2025-08-03 15:39:38 +0700

    Добавлена функция проверки

--- reminder.py
+++ reminder.py
@@ -5,11 +5,40 @@ import datetime
 import time
 import pygame
 
-wiwdow = Tk()
-wiwdow.title('Напоминание')
+t = 0
+
+def set():
+    global t
+    rem = sd.askstring('Время напоминания', 'Введите время напоминания в формате ЧЧ:ММ (в 24 часовом формате)')
+    if rem:
+        try:
+            hour = int(rem.split(':')[0])
+            minute = int(rem.split(':')[1])
+            now = datetime.datetime.now()
+            print(now)
+            dt = now.replace(hour=hour, minute=minute)
+            print(dt)
+            t = dt.timestamp()
+            print(t)
+        except Exception as e:
+            mb.showerror('Ошибка', f'Произошла ошибка')
+
+
+def check():
+    global t
+    if t:
+        now = time.time()
+        if now >= t:
+            play_snd()
+            t = 0
+    window.after(10000, check)
+
+
+window = Tk()
+window.title('Напоминание')
 label = Label(text='Установите напоминание')
 label.pack(pady=10)
-set_button = Button(text='Установить напоминание')
+set_button = Button(text='Установить напоминание', command=set)
 set_button.pack()
 
-wiwdow.mainloop()
+window.mainloop()
